1ef455c7ba6d092713b32b11b975c1f9d3d1d28a
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-27 20:31:58 -0800

    Update stoppingBehavior.js

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -2,9 +2,28 @@
 document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
 });
-
+$(document).ready(
+// When clicked, "More info" link should alert "Here's some info"
+// instead of going to a new webpage
+  function(){
+    const stopHREF = function(e){
+      e.preventDefault();
+      alert('More Info')
+    }
+    $('#more-info').click(stopHREF)
+    
+    
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+    
+    const meow = document.getElementById('cat')
+    const stopCat = function(e){
+      e.stopPropagation();
+      alert('Bow Wow!')
+    }
+    $('button').click(stopCat)
+  }
+)
