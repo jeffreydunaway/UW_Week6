a295ecee82d998eacf4d3e4c5e449e91bc2cc873
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-27 20:01:21 -0800

    Update jQueryToDoList.js

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,48 @@
 /**
  * Toggles "done" class on <li> element
  */
+$(document).ready(
+function(){
 
 /**
  * Delete element when delete link clicked
  */
+  /**
+   * Toggles "done" class on <li> element
+   */
+  const $li = $('li')
+  $li.each(function(index, element){
+    $(element).on('click',()=>{
+      $(element).toggleClass('done')
+    } )
+  })
+  /**
+   * Delete element when delete link clicked
+   */
+  $li.each((index, element)=>{
+    let test = $(element).children('a')
+    test.click(function(e){$(element).fadeOut("slow")})
+  })
+  
+    /**
+   * Adds new list item to <ul>
+   */
+  const addListItem = function(e) {
+    e.preventDefault()
+    const text = $('input').val();
+    console.log(text)
+    // rest here...
+    let newItem = $('<li>')
+    let newSpan = $('<span>').on('click',()=>{newItem.toggleClass('done')})
+    let newAtag =$('<a>')
+    newAtag.attr('class', 'delete')
+    newAtag.click(function(){newItem.fadeOut("slow")})
+    newAtag.text('Delete')
+    newSpan.text(text)
+    newItem.append(newSpan).append(newAtag).appendTo(".today-list")
+  };
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
-
-  // rest here...
-};
-
-// add listener for add
+  // add listener for add
+  $('.add-item').click(addListItem)
+}
+  ) 
