1dd0dbdf8f60fef07103050146c1aadb85182dab
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-27 19:56:09 -0800

    Update jQueryDomCrud.js

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -4,6 +4,12 @@
 $(document).ready(
     $('<a id="cta"></a>').text('Buy Now').insertAfter("p")
     )
+const $pTag = $("p")
+const $aTag = $('<a>')
+$aTag.attr('href', '')
+$aTag.attr('id', 'cta')
+$aTag.text('Buy Now')
+$aTag.insertAfter($pTag)
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
@@ -12,6 +18,9 @@ $(document).ready(
     console.log($('img').attr('data-color'))
 )
 
+const $img2 = $('img').attr('data-color')
+
+console.log($img2)
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
 
@@ -19,12 +28,14 @@ $(document).ready(
     $('ul li').last().addClass("highlight")
 )
 
+const $li = $('ul li').last().addClass("highlight")
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
 
 $(document).ready(
     $('p').remove()
 )
+$pTag.remove()
 
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
@@ -38,3 +49,6 @@ $(document).ready(
         )
 
 )
+$("#cta")[0].click(function(){
+    $('#cta').text('Test')
+})
